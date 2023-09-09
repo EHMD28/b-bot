e20ca5e32db552d62db8f4c47f6659d4486861ad
Bluehat Aspie <ehmdiagne0530@gmail.com>
2023-09-09 12:51:25 -0400

    added apps/utils/colors.py

---
+++ src/cli/apps/utils/colors.py
@@ -0,0 +1,133 @@
+FG_BLACK  = '\033[0;30m'
+FG_RED    = '\033[0;31m'
+FG_GREEN  = '\033[0;32m'
+FG_YELLOW = '\033[0;33m'
+FG_BLUE   = '\033[0;34m'
+FG_PURPLE = '\033[0;35m'
+FG_CYAN   = '\033[0;36m'
+FG_WHITE  = '\033[0;37m'
+
+FG_INTENSE_BLACK  = '\033[0;90m'
+FG_INTENSE_RED    = '\033[0;91m'
+FG_INTENSE_GREEN  = '\033[0;92m'
+FG_INTENSE_YELLOW = '\033[0;93m'
+FG_INTENSE_BLUE   = '\033[0;94m'
+FG_INTENSE_PURPLE = '\033[0;95m'
+FG_INTENSE_CYAN   = '\033[0;96m'
+FG_INTENSE_WHITE  = '\033[0;97m'
+
+FG_BOLD_BLACK  = '\033[1;30m'
+FG_BOLD_RED    = '\033[1;31m'
+FG_BOLD_GREEN  = '\033[1;32m'
+FG_BOLD_YELLOW = '\033[1;33m'
+FG_BOLD_BLUE   = '\033[1;34m'
+FG_BOLD_PURPLE = '\033[1;35m'
+FG_BOLD_CYAN   = '\033[1;36m'
+FG_BOLD_WHITE  = '\033[1;37m'
+
+FG_INTENSE_BOLD_BLACK  = '\033[1;90m'
+FG_INTENSE_BOLD_RED    = '\033[1;91m'
+FG_INTENSE_BOLD_GREEN  = '\033[1;92m'
+FG_INTENSE_BOLD_YELLOW = '\033[1;93m'
+FG_INTENSE_BOLD_BLUE   = '\033[1;94m'
+FG_INTENSE_BOLD_PURPLE = '\033[1;95m'
+FG_INTENSE_BOLD_CYAN   = '\033[1;96m'
+FG_INTENSE_BOLD_WHITE  = '\033[1;97m'
+
+UNDERLINE_BLACK  = '\033[4;30m'
+UNDERLINE_RED    = '\033[4;31m'
+UNDERLINE_GREEN  = '\033[4;32m'
+UNDERLINE_YELLOW = '\033[4;33m'
+UNDERLINE_BLUE   = '\033[4;34m'
+UNDERLINE_PURPLE = '\033[4;35m'
+UNDERLINE_CYAN   = '\033[4;36m'
+UNDERLINE_WHITE  = '\033[4;37m'
+
+BG_BLACK  = '\033[40m'
+BG_RED    = '\033[41m'
+BG_GREEN  = '\033[42m'
+BG_YELLOW = '\033[43m'
+BG_BLUE   = '\033[44m'
+BG_PURPLE = '\033[45m'
+BG_CYAN   = '\033[46m'
+BG_WHITE  = '\033[47m'
+
+BG_INTENSE_BLACK  = '\033[0;100m'
+BG_INTENSE_RED    = '\033[0;101m'
+BG_INTENSE_GREEN  = '\033[0;102m'
+BG_INTENSE_YELLOW = '\033[0;103m'
+BG_INTENSE_BLUE   = '\033[0;104m'
+BG_INTENSE_PURPLE = '\033[0;105m'
+BG_INTENSE_CYAN   = '\033[0;106m'
+BG_INTENSE_WHITE  = '\033[0;107m'
+
+COLOR_RESET = '\033[0m'
+
+termcolors = {
+    'fg-black':  FG_BLACK,
+    'fg-red':    FG_RED,
+    'fg-green':  FG_GREEN,
+    'fg-yellow': FG_YELLOW,
+    'fg-blue':   FG_BLUE,
+    'fg-purple': FG_PURPLE,
+    'fg-cyan':   FG_CYAN,
+    'fg-white':  FG_WHITE,
+    
+    'fg-i-black':  FG_INTENSE_BLACK,
+    'fg-i-red':    FG_INTENSE_RED,
+    'fg-i-green':  FG_INTENSE_GREEN,
+    'fg-i-yellow': FG_INTENSE_YELLOW,
+    'fg-i-blue':   FG_INTENSE_BLUE,
+    'fg-i-purple': FG_INTENSE_PURPLE,
+    'fg-i-cyan':   FG_INTENSE_CYAN,
+    'fg-i-white':  FG_INTENSE_WHITE,
+
+    'fg-b-black':  FG_BOLD_BLACK,
+    'fg-b-red':    FG_BOLD_RED,
+    'fg-b-green':  FG_BOLD_GREEN,
+    'fg-b-yellow': FG_BOLD_YELLOW,
+    'fg-b-blue':   FG_BOLD_BLUE,
+    'fg-b-purple': FG_BOLD_PURPLE,
+    'fg-b-cyan':   FG_BOLD_CYAN,
+    'fg-b-white':  FG_BOLD_WHITE,
+
+    'fg-ib-black':  FG_INTENSE_BOLD_BLACK,
+    'fg-ib-red':    FG_INTENSE_BOLD_RED,
+    'fg-ib-green':  FG_INTENSE_BOLD_GREEN,
+    'fg-ib-yellow': FG_INTENSE_BOLD_YELLOW,
+    'fg-ib-blue':   FG_INTENSE_BOLD_BLUE,
+    'fg-ib-purple': FG_INTENSE_BOLD_PURPLE,
+    'fg-ib-cyan':   FG_INTENSE_BOLD_CYAN,
+    'fg-ib-white':  FG_INTENSE_BOLD_WHITE,
+    
+    'underline-black':  UNDERLINE_BLACK,
+    'underline-red':    UNDERLINE_RED,
+    'underline-green':  UNDERLINE_GREEN,
+    'underline-yellow': UNDERLINE_YELLOW,
+    'underline-blue':   UNDERLINE_BLUE,
+    'underline-purple': UNDERLINE_PURPLE,
+    'underline-cyan':   UNDERLINE_CYAN,
+    'underline-white':  UNDERLINE_WHITE,
+    
+    'bg-black':  BG_BLACK,
+    'bg-red':    BG_RED,
+    'bg-green':  BG_GREEN,
+    'bg-yellow': BG_YELLOW,
+    'bg-blue':   BG_BLUE,
+    'bg-purple': BG_PURPLE,
+    'bg-cyan':   BG_CYAN,
+    'bg-white':  BG_WHITE,
+
+    'bg-i-black':  BG_INTENSE_BLACK,
+    'bg-i-red':    BG_INTENSE_RED,
+    'bg-i-green':  BG_INTENSE_GREEN,
+    'bg-i-yellow': BG_INTENSE_YELLOW,
+    'bg-i-blue':   BG_INTENSE_BLUE,
+    'bg-i-purple': BG_INTENSE_PURPLE,
+    'bg-i-cyan':   BG_INTENSE_CYAN,
+    'bg-i-white':  BG_INTENSE_WHITE,
+    
+    'color-reset': COLOR_RESET,
+
+}
+
