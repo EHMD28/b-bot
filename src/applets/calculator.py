8084c14ed5f449e7af0775e651f6a4e9bcfc0795
BluehatAspie <ehmdiagne0530@gmail.com>
2024-07-19 15:21:15 -0400

    began implemented calc applet

--- src/applets/calculator.py
+++ src/applets/calculator.py
@@ -1,14 +1,26 @@
 from bbot_core.applets import Applet
 
 
+def evaluate_expression() -> None: ...
+
+
+def calc_area() -> None: ...
+
+
+def calc_volume() -> None: ...
+
+
+def convert_units() -> None: ...
+
+
 def start_calc_applet() -> None:
     user_inp: str = ""
     while user_inp not in ["exit", "quit"]:
         user_inp = input("calculator> ")
+
     
 # Calculator Applet for Export
 calc_applet = Applet(name="Calculator",
                      description="an applet for preforming arithmetic operations",
                      keywords=["calculator", "math"],
                      start_func=start_calc_applet)
-    
